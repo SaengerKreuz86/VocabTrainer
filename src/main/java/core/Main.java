eacb5cd335c6880d96a182327c7cc87c7c10eedf
demmin <joel.demmin02@gmail.com>
2025-07-05 11:15:36 +0200

    Refactor: formatting in console

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
     private static final String INTRO =
