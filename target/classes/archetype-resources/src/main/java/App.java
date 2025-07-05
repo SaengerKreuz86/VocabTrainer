ea109954885c84d9f805bf030172b876e8bc921e
Lukas Langrock <lukas.langrock@outlook.de>
2025-07-05 11:49:06 +0200

    Add: L6

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.demmin.vocab_trainer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
